de51192ff7222ea939882f6b6e36a0f073d73833
Paul Roper <p.s.roper@warwick.ac.uk>
2013-10-21 00:18:20 +0100

    Refactored code to fit Java/Android coding standards.

--- sMessage/src/com/csulcv/smessage/MainActivity.java
+++ sMessage/src/com/csulcv/smessage/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.csulcv.smessage;
 
 import android.app.Activity;
@@ -9,32 +10,31 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	public final static String EXTRA_MESSAGE = "com.csulcv.smessage.MESSAGE";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    public final static String EXTRA_MESSAGE = "com.csulcv.smessage.MESSAGE";
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	/* Called when the user clicks the Send button */
-	public void sendMessage(View view) {
-		
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		
-		startActivity(intent);
-		
-		
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+    }
 
 }
