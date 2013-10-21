4c662fa60797bf180e2c00fc0891756246e12cdf
Paul Roper <p.s.roper@warwick.ac.uk>
2013-10-21 12:33:58 +0100

    Started implementing action bar.

--- sMessage/src/com/csulcv/smessage/MainActivity.java
+++ sMessage/src/com/csulcv/smessage/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     public final static String EXTRA_MESSAGE = "com.csulcv.smessage.MESSAGE";
 
@@ -21,8 +21,23 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /** Called when the user clicks the Send button */
